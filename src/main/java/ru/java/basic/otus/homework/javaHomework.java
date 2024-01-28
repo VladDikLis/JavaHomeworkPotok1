3155f4c9020cc91b406309151cff9763ae32ae2f
VladLisitsyn <vladlisitsindiklis@yandex.ru>
2024-01-28 16:02:32 +0800

    Initial commit

---
+++ src/main/java/ru/java/basic/otus/homework/javaHomework.java
@@ -0,0 +1,11 @@
+package ru.java.basic.otus.homework;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+public class javaHomework {
+    public static void main(String[] args) throws InterruptedException {
+
+
+    }
+}
